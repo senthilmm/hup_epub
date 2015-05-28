c67050cda30faf0c447c6e4c2378e5cddd32920c
Cholfin, Bryan <bryan_cholfin@harvard.edu>
2015-05-28 10:32:23 -0400

    remove test class. fix attr list processing.

--- hup-epub-test/epub-config-test.py
+++ hup-epub-test/epub-config-test.py
@@ -3,12 +3,12 @@ import zipfile
 import re
 from bs4 import BeautifulSoup, SoupStrainer
 
-tag_list = [{'tag': 'body', 'attrs': {'epub:type': 'bodymatter'}},
-            {'tag': 'article', 'attrs': {'role': 'main', 'epub:type': 'chapter'}},
-            {'tag': 'img', 'attrs': {'id': '', 'alt': ''}},
+tag_list = [{'tag': 'body', 'attrs': {'epub:type': ['bodymatter']}},
+            {'tag': 'article', 'attrs': {'role': ['main'], 'epub:type': ['chapter']}},
+            {'tag': 'img', 'attrs': {'id': [''], 'alt': ['']}},
             {'tag': 'blockquote', 'attrs': {'class': ['epigraph', 'extract']}},
-            {'tag': 'tr', 'attrs': {'role': 'row'}},
-            {'tag': 'h1', 'attrs': {'class': 'head'}},
+            {'tag': 'tr', 'attrs': {'role': ['row']}},
+            {'tag': 'h1', 'attrs': {'class': ['head']}},
             ]
 
 
@@ -39,89 +39,98 @@ def get_metadata(opf):
         print("No author found")
 
 
-class Test:
-    """test suite for html files in epub"""
-    def __init__(self, files, epub, output_filename):
-        """set up files for tests"""
-        self.out_file = open(output_filename, 'w')
-        self.html_tests(files, epub)
+def html_tests(files, epub, messages):
+    """For each html file, make the Soup and pass it to the tests."""
+    nuts_file = [fname for fname in files if "note" in fname]  # find notes.html
+    nuts = BeautifulSoup(epub.open(nuts_file[0])).select("a[id]")  # make soup from notes
+    for html in files:
+        file_test = html.rsplit('/', maxsplit=1)[1]
+        messages.append("\n"+"{:-^60}".format(file_test)+"\n")
+        soup = BeautifulSoup(epub.open(html))
+        check_tags(soup, tag_list, messages)
+        text_list = ["chapter", "intro", "preface"]
+        for text in text_list:
+            if text in file_test:
+                check_notes(soup, nuts, file_test, messages)
+    return messages
 
-    def html_tests(self, files, epub):
-        """For each html file, make the Soup and pass it to the tests."""
-        nuts_file = [fname for fname in files if "note" in fname]  # find notes.html
-        nuts = BeautifulSoup(epub.open(nuts_file[0])).select("a[id]")  # make soup from notes
-        for html in files:
-            file_test = html.rsplit('/', maxsplit=1)[1]
-            self.out_file.write("\n"+"{:-^60}".format(file_test)+"\n")
-            soup = BeautifulSoup(epub.open(html))
-            self.check_tags(soup, tag_list)
-            text_list = ["chapter", "intro", "preface"]
-            for text in text_list:
-                if text in file_test:
-                    self.check_notes(soup, nuts, file_test)
-        self.out_file.close()
 
-    def check_tags(self, soup, tags):
-        for tag in tags:
-            tag_soup = soup.find_all(tag.get("tag"))
-            for element in tag_soup:
-                attr_exist = self.check_attr_exist(element, tag["attrs"])
-                if attr_exist:
-                    self.check_attr_values(element, tag["attrs"])
+def check_tags(soup, tags, messages):
+    for tag in tags:
+        tag_soup = soup.find_all(tag.get("tag"))
+        for element in tag_soup:
+            attr_exist, messages = check_attr_exist(element, tag["attrs"], messages)
+            print(attr_exist)
+            if attr_exist:
+                check_attr_values(element, tag["attrs"], messages)
+    return messages
 
-    def check_figure(self, soup):
-        """Examine <figure> tags. Check if fallbacks are needed."""
-        figures = soup.find_all('figure')
-        for figure in figures:
-            if figure.table:
-                if figure.img:
-                    self.check_attr_values(figure.img, "class", "fallback")
-                else:
-                    self.out_file.write("no fallback image\n")
 
-    def check_attr_exist(self, tag, attrs):
-        """Check attribute attr exists for tag."""
-        for attr in attrs.keys():
-            print(attr, tag.name, tag.attrs)
-            if attr not in tag.attrs:
-                self.out_file.write(attr+" not found for "+tag.name+"\n")
-                return False
+def check_figure(soup, messages):
+    """Examine <figure> tags. Check if fallbacks are needed."""
+    figures = soup.find_all('figure')
+    for figure in figures:
+        if figure.table:
+            if figure.img:
+                check_attr_values(figure.img, "class", "fallback", messages)
             else:
-                return True
+                messages.append("no fallback image\n")
+    return messages
 
-    def check_attr_values(self, tag, attrs):
-        """check that attribute has allowed value"""
-        for attr, values in attrs.items():
-            print(attr, values, tag.name, tag.attrs)
-            for value in values:
-                if value not in tag.attrs[attr]:
-                    self.out_file.write(attr+" value "+value+" missing or incorrect\n")
 
-    def check_alt_text(self, soup):
-        """Check <img> tags for alt-text"""
-        images = soup.find_all('img')
-        for image in images:
-            if ('alt' not in image.attrs) or (image['alt'] == "image") or (image['alt'] == ""):
-                self.out_file.write("no alt text for "+image.name+"\n")
+def check_attr_exist(tag, attrs, messages):
+    """Check attribute attr exists for tag."""
+    for attr in attrs.keys():
+        print("a: ",attr, "t: ", tag.name, tag.attrs)
+        if attr not in tag.attrs:
+            messages.append(attr+" not found for "+tag.name+"\n")
+            return False, messages
+        else:
+            return True, messages
 
-    def check_notes(self, soup, footnotes, file):
-        """Examine callout and notes. Check that notes and links work both ways."""
-        links = soup.select('a[href^="note"]')
-        for link in links:
-            l = link['href'].partition('#')[2]
-            match = [footnote for footnote in footnotes if footnote['id'] == l]
-            if not match[0]:
-                self.out_file.write("no match for "+l+"\n")
-            # except KeyError:
+
+def check_attr_values(tag, attrs, messages):
+    """check that attribute has allowed value"""
+    for attr, values in attrs.items():
+        print(attr, values, tag.name, tag.attrs)
+        for value in values:
+            if value not in tag.attrs[attr]:
+                messages.append(attr+" value "+value+" missing or incorrect\n")
+    return messages
+
+
+def check_alt_text(soup, messages):
+    """Check <img> tags for alt-text"""
+    images = soup.find_all('img')
+    for image in images:
+        if ('alt' not in image.attrs) or (image['alt'] == "image") or (image['alt'] == ""):
+            messages.append("no alt text for "+image.name+"\n")
+    return messages
+
+
+def check_notes(soup, footnotes, file, messages):
+    """Examine callout and notes. Check that notes and links work both ways."""
+    links = soup.select('a[href^="note"]')
+    for link in links:
+        l = link['href'].partition('#')[2]
+        match = [footnote for footnote in footnotes if footnote['id'] == l]
+        if not match[0]:
+            messages.append("no match for "+l+"\n")
+    return messages
+        # except KeyError:
 
 
 def main(file):
     epub = zipfile.ZipFile(file, 'r')
+    messages = []
     output_filename = file.rsplit('.', maxsplit=1)[0]+".txt"
     html_files, css_files, opf_file = parse_file_list(epub)
     get_metadata(epub.open(opf_file[0]))
-    Test(html_files, epub, output_filename)
-
+    result = html_tests(html_files, epub, messages)
+    print(result)
+    with open(output_filename, 'w') as results:
+        for line in result:
+            print(line)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
