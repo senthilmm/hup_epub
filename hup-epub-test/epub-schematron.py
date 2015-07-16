27a92adda08f74947d5771b1a5631b32e48425f1
Cholfin, Bryan <bryan_cholfin@harvard.edu>
2015-07-16 14:17:47 -0400

    epub-schematron now using lxml, BS4 removed

--- hup-epub-test/epub-schematron.py
+++ hup-epub-test/epub-schematron.py
@@ -1,8 +1,8 @@
 import argparse
 import zipfile
 import re
-from bs4 import BeautifulSoup, SoupStrainer
-from lxml import etree, isoschematron
+from lxml import etree, isoschematron, objectify
+import lxml.html as lh
 
 import logging
 
@@ -19,38 +19,27 @@ def parse_file_list(file):
 
 def get_metadata(opf, messages):
     """Find title and author metadata."""
-    metadata = SoupStrainer("metadata")
-    content = BeautifulSoup(opf, "xml", parse_only=metadata)
-    try:
-        messages.append('Title: {0}\n'.format(content.title.contents[0].string))
-    except AttributeError:
-        messages.append("No author found\n")
-    try:
-        sub = content.find(id="subtitle").string
-        messages.append("Subtitle: "+sub+"\n")
-    except AttributeError:
-        messages.append("No subtitle found\n")
-    try:
-        messages.append('Author: {0}\n'.format(content.creator.contents[0].string))
-    except:
-        messages.append("No author found\n")
+    meta_data = objectify.parse(opf).getroot()
+    meta_list = meta_data.findall('.//{http://www.idpf.org/2007/opf}meta')
+    dc_list = meta_data.find('.//{http://www.idpf.org/2007/opf}metadata')
+    for child in dc_list.iterchildren():
+        print(child.xpath('local-name()'), child.text)
 
 
 def html_tests(files, epub, messages):
     """For each html file, make the Soup and pass it to the tests."""
     nuts_file = [fname for fname in files if "note" in fname]  # find notes.html
-    nuts = BeautifulSoup(epub.open(nuts_file[0])).select("a[id]")  # make soup from notes
+    notes = epub.extract(nuts_file[0])
+    note_file = lh.parse(notes).getroot().findall(".//a[@id]")
     for html in files:
         file_to_test = html.rsplit('/', maxsplit=1)[1]
         messages.append("\n"+"{:-^60}".format(file_to_test)+"\n")
         html_file = epub.extract(html)
-        soup_ingredients = epub.open(html)
-        soup = BeautifulSoup(soup_ingredients)
         check_tags(html_file, messages)
         note_list = ["chapter", "intro", "preface"]
         for name in note_list:
             if name in file_to_test:
-                check_notes(soup, nuts, file_to_test, messages)
+                check_notes(html_file, note_file, file_to_test, messages)
     return messages
 
 
@@ -63,25 +52,35 @@ def check_tags(html, messages):
     print(html, report.xpath("//text()"))
 
 
-
-def check_notes(soup, footnotes, file, messages):
-    """Examine callout and notes. Check that notes and links work both ways."""
-    links = soup.select('a[href^="note"]')
-    for link in links:
-        l = link['href'].partition('#')[2]
-        match = [footnote for footnote in footnotes if footnote['id'] == l]
-        if not match[0]:
-            messages.append("no match for "+l+"\n")
-    return messages
-        # except KeyError:
-
+def check_notes(html, notes, file_to_test, messages):
+    chapter = lh.parse(html).getroot().findall(".//a[@href]")
+    # note_refs = lh.iterlinks(notes)
+    for link in chapter:
+        if link.attrib['href'].startswith('note'):
+            part = link.attrib['href'].partition('#')[2]
+            match = [ref.tag for ref in notes if ref.attrib['id'] == part]
+            if not match[0]:
+                print('no match for', link[0].attrib['id'] )
+            print(match[0])
+    for ref in notes:
+        chap_refs = [link for link in chapter if 'id' in link.attrib]
+        try:
+            target = ref.attrib['href'].partition('#')
+            if target[0] == file_to_test:
+                match = [link.tag for link in chap_refs if link.attrib['id'] == target[2]]
+                if match[0]:
+                    print('match:', match[0])
+                else:
+                    print('no match')
+        except KeyError:
+            print('No href attribute')
 
 def main(file):
     epub = zipfile.ZipFile(file, 'r')
     messages = []
     output_filename = file.rsplit('.', maxsplit=1)[0]+".txt"
     html_files, css_files, opf_file = parse_file_list(epub)
-    get_metadata(epub.open(opf_file[0]), messages)
+    get_metadata(epub.extract(opf_file[0]), messages)
     result = html_tests(html_files, epub, messages)
 #    logging.debug(repr(result))
     with open(output_filename, 'w') as results:
